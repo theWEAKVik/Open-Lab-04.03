05ebddfef31b62fd2f8f4a711f379b73192ae3f7
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-26 18:50:38 +0200

    Created tests

---
+++ Open-Lab-04.03/StringTools.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_04._03
+{
+    public class StringTools
+    {
+        public string RemoveFirstLast(string original)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-04.03/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Open_Lab_04._03
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private StringTools tools;
+
+        private const int RandStrMinSize = 1;
+        private const int RandStrMaxSize = 15;
+        private const int RandSeed = 403403403;
+        private const int RandTestCasesCount = 95;
+
+        [OneTimeSetUp]
+        public void Init() => tools = new StringTools();
+
+        [TestCase("hello", "ell")]
+        [TestCase("maybe", "ayb")]
+        [TestCase("benefit", "enefi")]
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCaseSource(nameof(GetRandom))]
+        public void RemoveFirstLastTest(string str, string expected) =>
+            Assert.That(tools.RemoveFirstLast(str), Is.EqualTo(expected));
+
+        private static IEnumerable GetRandom()
+        {
+            var rand = new Random(RandSeed);
+
+            for (var i = 0; i < RandTestCasesCount; i++)
+            {
+                var arr = new char[rand.Next(RandStrMinSize, RandStrMaxSize + 1)];
+
+                for (var j = 0; j < arr.Length; j++)
+                    arr[j] = (char) rand.Next('a', 'z' + 1);
+
+                yield return new TestCaseData(new string(arr), new string(arr.Length > 1 ? arr[1..^1] : arr));
+            }
+        }
+
+    }
+}
